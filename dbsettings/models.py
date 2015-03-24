a5e74bfc4d2ef45bda700c60e19b007ed616969c
Snakeyyy <m.majsky@gmail.com>
2015-03-24 16:10:57 +0100

    remove Sites

--- dbsettings/models.py
+++ dbsettings/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from django.contrib.sites.models import Site
+#from django.contrib.sites.models import Site
 
 
 class SettingManager(models.Manager):
     def get_query_set(self):
         all = super(SettingManager, self).get_query_set()
-        return all.filter(site=Site.objects.get_current())
+        return all
 
 
 class Setting(models.Model):
-    site = models.ForeignKey(Site)
+    #site = models.ForeignKey(Site)
     module_name = models.CharField(max_length=255)
     class_name = models.CharField(max_length=255, blank=True)
     attribute_name = models.CharField(max_length=255)
@@ -21,5 +21,5 @@ class Setting(models.Model):
         return self.pk is not None
 
     def save(self, *args, **kwargs):
-        self.site = Site.objects.get_current()
+        #self.site = Site.objects.get_current()
         return super(Setting, self).save(*args, **kwargs)
